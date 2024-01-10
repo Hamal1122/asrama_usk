151cfe767905e2a087d60724b1f464381710b727
Afkar <afkarsiddiq0@gmail.com>
2024-01-10 22:58:10 +0700

    riwayat table

---
+++ database/migrations/2024_01_10_143049_riwayat.php
@@ -0,0 +1,36 @@
+<?php
+
+use GuzzleHttp\Psr7\DroppingStream;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('riwayat', function(Blueprint $table){
+            $table->id();
+            $table->ForeignId('user_id');
+            $table->ForeignId('kamar_id');
+            $table->ForeignId('berkas_id');
+            $table->date('tanggal_masuk');
+            $table->date('tanggal_keluar');
+            $table->string('kategori');
+            $table->string('bukti_pembayaran');
+            $table->integer('status'); //0 =menunggu, 1 =aktif 2= riwayat/expired
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('riwayat');
+    }
+};
